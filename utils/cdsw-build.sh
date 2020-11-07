3428c8debb2aac62fa9d332be143e620978a57e5
EC2 Default User <ec2-user@ip-172-16-81-184.eu-west-1.compute.internal>
2020-11-07 16:41:40 +0000

    refactor for sm

--- utils/cdsw-build.sh
+++
@@ -1,5 +0,0 @@
-pip3 install -r /home/cdsw/utils/requirements3.txt
-python3 -m spacy download en
-
-#mkdir /home/cdsw/R
-#Rscript /home/cdsw/utils/install.R
---
+++ utils/lifecycle-config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+#git clone https://github.com/fastforwardlabs/airline-sentiment
+
+pip install torchtext==0.3.1 umap umap-learn holoviews dash dash-ui #spacy==2.1.4 
+
+#python3 -m spacy download en
+
+#Rscript install.packages(c("ggmap", "leaflet", "tidytext", "wordcloud", "aws.s3"))
